3a3a3b3e1c3883ca0ae3a4f1c631e65ac659354c
niuyuxiao <niuyuxiao@139.com>
2017-03-26 20:03:50 +0800

    Add files via upload

---
+++ install_lap.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# install Apache and PHP (in a loop because a lot of installs happen
+# on VM init, so won't be able to grab the dpkg lock immediately)
+ apt-get -y update && apt-get -y install apache2 php 
+
+
+# write some PHP; these scripts are downloaded beforehand as fileUris
+cp index.php /var/www/html/
+cp do_work.php /var/www/html/
+rm /var/www/html/index.html
+# restart Apache
+apachectl restart
